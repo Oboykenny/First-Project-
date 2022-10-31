6ec1560d2fb424145bd47fe852a74b0f38a222b1
Oboykenny <117093370+Oboykenny@users.noreply.github.com>
2022-10-31 14:02:00 +0000

    Initial commit

---
+++ catch up notes for week 5 (6).R
@@ -0,0 +1,15 @@
+library(tidyverse)
+library(gapminder)
+
+# filter for values in Europe and the year 2007
+gapminder %>%
+pivot_longer(lifeExp:gdpPercap) %>%
+filter(str_detect(continent, "Europe"),
+       year > 1987)  %>%
+         ggplot() +
+         aes(x = year,
+             y = value,
+             colour = name) + 
+         geom_line() +
+         facet_wrap(~ country)
+)       
